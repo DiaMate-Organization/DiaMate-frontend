b53136c08671599eb4785628fd57a1bc11cc6666
Ryuugamine <yoerdanatan@gmail.com>
2025-05-19 10:17:10 +0700

    chore : make article section component

--- app/page.jsx
+++ app/page.jsx
@@ -18,30 +18,16 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+
 import { benefits, data, faqs, teams, univ } from "@/lib/data";
 import { cn } from "@/lib/utils";
-import {
-  ArrowRightIcon,
-  ChartColumnBig,
-  InfoIcon,
-  TriangleAlertIcon,
-} from "lucide-react";
+
 import Image from "next/image";
 import { AuroraBackground } from "@/components/ui/aurora-background";
-import {
-  GitHubLogoIcon,
-  InstagramLogoIcon,
-  LinkedInLogoIcon,
-} from "@radix-ui/react-icons";
+
 import Link from "next/link";
 import { Spotlight } from "@/components/ui/spotlight-new";
+import ArticleSection from "@/components/ArticleSection";
 
 export default function Home() {
   return (
@@ -221,43 +207,7 @@ export default function Home() {
         </h2>
 
         <div className="mt-12 flex flex-col items-center justify-center px-5 md:p-0">
-          <Carousel className="w-full max-w-7xl">
-            <CarouselContent className="-ml-1 gap-8">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem
-                  key={index}
-                  className="pl-1 md:basis-1/2 lg:basis-1/3"
-                >
-                  <div className="p-1">
-                    <div className={"border-0 bg-transparent"}>
-                      <Image
-                        width={500}
-                        height={500}
-                        src={"/article-1.jpg"}
-                        className="rounded-md"
-                        alt="articles iamge"
-                      />
-                      <div className="flex flex-col items-start py-2 px-2">
-                        <h3 className="text-2xl font-semibold">
-                          <a href="#">
-                            Makanan sehat yang bisa dimakan saat diabetes
-                          </a>
-                        </h3>
-
-                        <div className="flex mt-5 text-muted-foreground w-full items-center justify-between">
-                          <p>Dewi Persik</p>
-                          <p>6 Menit</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
+          <ArticleSection />
         </div>
       </div>
 
